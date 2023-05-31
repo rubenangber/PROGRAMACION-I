aebd056fe47468611319cc6717e36463d2a742f1
rubenangber <rubigrowl@gmail.com>
2023-05-31 16:31:19 +0200

    Update

--- rubihelp.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void fillVector(int *vector, int tam);
-void seeVector(int *vector, int tam);
-void fillMatriz(int **matriz, int filas, int columnas);
-void seeMatriz(int **matriz, int filas, int columnas);
